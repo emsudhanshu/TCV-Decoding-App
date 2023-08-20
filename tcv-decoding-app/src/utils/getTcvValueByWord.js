6eaaf6dd0409cf5febf3c30c4f09f3a8fd8c0ceb
Sudhanshu Kakkar <74782471+emsudhanshu@users.noreply.github.com>
2023-08-20 13:03:43 +0530

    added optional chaining operator for error handling in  getTcvValueByWord.js

--- tcv-decoding-app/src/utils/getTcvValueByWord.js
+++ tcv-decoding-app/src/utils/getTcvValueByWord.js
@@ -3,7 +3,7 @@ import tcvValueMapping from "./tcvValueMapping";
 const getTcvValueByWord = word => {
     let sum = 0;
     word?.split('')?.forEach(char=>{
-        sum = sum + (tcvValueMapping[char] ? tcvValueMapping[char] : 0)
+        sum = sum + (tcvValueMapping?.[char] ? tcvValueMapping?.[char] : 0)
     })
     return sum
 }
