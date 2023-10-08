261c461a190616f1289e64b3ab724c09f541db84
emsudhanshu <kakkar.sudhanshu@tcs.com>
2023-10-08 16:47:30 +0530

    correct TCV values

--- tcv-decoding-app/src/utils/getTcvValueByWord.js
+++ tcv-decoding-app/src/utils/getTcvValueByWord.js
@@ -3,10 +3,19 @@ import tcvValueMapping from "./tcvValueMapping";
 const getTcvValueByWord = word => {
     let sum = 0;
     let valueBreakup = "";
+    let prevChar = '';
     word?.split('')?.forEach(char=>{
         console.log(char, tcvValueMapping?.[char])
         valueBreakup = valueBreakup + ' ' + `(${char})(${tcvValueMapping?.[char]})`;
-        sum = sum + (tcvValueMapping?.[char] ? tcvValueMapping?.[char] : 0)
+
+        if(prevChar == '्' && char == "र"){
+            sum = sum + (tcvValueMapping?.[char] ? (tcvValueMapping?.[char] - 2) : 0);
+        }
+        else{
+            sum = sum + (tcvValueMapping?.[char] ? tcvValueMapping?.[char] : 0);
+        }
+
+        prevChar = char;
     })
     console.log("---- word",word)
     console.log("---- tcv value", sum);
